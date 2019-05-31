5218cc13340bc8a70d74ab878777dea7a8a80d75
Nhan Thai (dandoh) <dainhan605@gmail.com>
2019-05-30 22:02:49 -0400

    running Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,31 @@ module Main where
 import Expression
 import Operations
 
+-- ℝ^5 vector
 x :: Expression OneD
 x = var1d 5 "x"
 
-c :: Expression R
-c = var "c"
+-- ℝ
+r :: Expression R
+r = var "r"
 
-z = c |*| x
+-- ℂ^5
+xc :: Expression OneDC
+xc = var1dc 5 "xc"
+
+-- ℂ
+c :: Expression C
+c = varc "c"
 
 main :: IO ()
-main = print $ z
+main = do
+    print x
+    print r
+    print xc
+    print c
+    print $ x |+| x
+    print $ xc |+| xc
+    print $ r |*| x
+    print $ r |*| xc
+    print $ r |*| c
+
